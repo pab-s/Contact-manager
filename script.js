3dfdba742bc4c2090b35cf02618f81b715cc3045
Pablo Jurado <pjurado85@gmail.com>
2016-09-15 22:21:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var sec2 = document.getElementById('section2');
+var sec3 = document.getElementById('section3');
+var sec4 = document.getElementById('section4');
+
+sec2.style.display = "none";
+sec3.style.display = "none";
+sec4.style.display = "none";
