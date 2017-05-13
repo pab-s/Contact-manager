cd55062eb45269fefeff8fb475f4cfb83ebfc840
Pablo Jurado <pjurado85@gmail.com>
2017-05-13 14:41:40 -0500

    add event listener for checkbox

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* global localStorage */
+/* global localStorage, $ */
 // inputs
 var inputName = document.getElementById('inputName')
 var inputPhone = document.getElementById('inputPhone')
@@ -6,29 +6,22 @@ var inputEmail = document.getElementById('inputEmail')
 var feedback = document.getElementById('feedback')
 
 // buttons
-var btnAdd = document.getElementById('btnAdd')
-var btnDelete = document.getElementById('btnDelete')
-var btnSave = document.getElementById('btnSave')
+var btnAddContact = document.getElementById('btnAdd')
+var btnSaveEdit = document.getElementById('btnSave')
 
 // tables
 var tableSearch = document.getElementById('tableSearch')
 var tableDelete = document.getElementById('tableDelete')
 var tableEdit = document.getElementById('tableEdit')
 
-var arrayContact = []
-
 // local storage setup
 var myStorage = localStorage
-var contactNum = myStorage.length
 
-// convert object contact to JSON string and storage it
-function uploadContact (key, value) {
-  var contactStr = JSON.stringify(value)
-  myStorage.setItem(key, contactStr)
-}
+let arrayContact = downloadContact(localStorage)
+let isCheckbox = true
 
 // object constructor
-function Contact (name, phone, email, id) {
+function Contact (name, phone, email) {
   this.name = name
   this.phone = phone
   this.email = email
@@ -40,85 +33,50 @@ function Contact (name, phone, email, id) {
   }
 };
 
-function resetContact () {
-  for (var i = 0; i < arrayContact.length; i++) {
-    uploadContact(i, arrayContact[i])
-  }
-};
-
-// input listener
-btnAdd.addEventListener('click', function (e) {
+function addContact (e) {
   var validEmail = /\S+@\S+\.\S+/
-  var validPhone = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/
+  var validPhone = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\]{0,1}[0-9]{3}[-\s\]{0,1}[0-9]{4}$/
   var validName = /\D/
 
   if (validName.test(inputName.value) && validPhone.test(inputPhone.value) && validEmail.test(inputEmail.value)) {
-    var newContact = new Contact(inputName.value, inputPhone.value, inputEmail.value, contactNum)
-    arrayContact[contactNum] = newContact
-
-    // log the new contact
-    arrayContact[contactNum].date = arrayContact[contactNum].date()
-    uploadContact(contactNum, arrayContact[contactNum])
-
-    // change contact num and resets input box;
-    contactNum += 1
+    var newContact = new Contact(inputName.value, inputPhone.value, inputEmail.value)
+    arrayContact.push(newContact)
+    updateContactStorage(arrayContact)
   } else {
     e.preventDefault()
+    var str = ''
+    if (!validName.test(inputName.value)) str += 'name is wrong <br>'
+    if (!validPhone.test(inputPhone.value)) str += 'phone is wrong <br>'
+    if (!validEmail.test(inputEmail.value)) str += 'email is wrong <br>'
+    feedback.innerHTML = str
     $('#modalPop').modal('show')
-
-    var msgName = ''
-    var msgPhone = ''
-    var msgEmail = ''
-    var finalMsg
-
-    if (!validName.test(inputName.value)) {
-      msgName = 'name is wrong'
-    }
-    if (!validPhone.test(inputPhone.value)) {
-      msgPhone = 'phone is wrong'
-    }
-    if (!validEmail.test(inputEmail.value)) {
-      msgEmail = 'email is wrong'
-    }
-
-    finalMsg = msgName + '<br>' + msgPhone + '<br>' + msgEmail
-    feedback.innerHTML = finalMsg
   }
-})
+}
 
-btnDelete.addEventListener('click', function () {
-  var allCheckBoxes = tableDelete.querySelectorAll('input')
-  for (var i = allCheckBoxes.length; i > 0; i--) {
-    if (allCheckBoxes[i - 1].checked) {
-      arrayContact.splice(i - 1, 1)
-    }
-  }
+function saveEdit () {
+  // var nameX
+  // var dataX
+  // var data = tableEdit.querySelectorAll('tr')
+  //
+  // for (var i = 0; i < data.length; i++) {
+  //   for (var j = 0; j < data[i].children.length; j++) {
+  //     nameX = data[i].children[j].id
+  //     dataX = data[i].children[j].innerHTML
+  //     arrayContact[i][nameX] = dataX; 8
+  //   }
+  // }
+  // localStorage.clear()
+  // resetContact()
+
+}
+
+// convert object contact to JSON string and storage it
+function updateContactStorage (arr) {
   localStorage.clear()
-  resetContact()
-})
-
-btnSave.addEventListener('click', function (e) {
-  var nameX
-  var dataX
-  var data = tableEdit.querySelectorAll('tr')
-
-  for (var i = 0; i < data.length; i++) {
-    for (var j = 0; j < data[i].children.length; j++) {
-      nameX = data[i].children[j].id
-      dataX = data[i].children[j].innerHTML
-      arrayContact[i][nameX] = dataX; 8
-    }
-  }
-  localStorage.clear()
-  resetContact()
-})
-
-// window.onload
-
-// ------------------------------------------ //
-
-let usersArr = downloadContact(localStorage)
-let checkbox = '<input type="checkbox" id="check1">'
+  arr.forEach((item, index) => {
+    myStorage.setItem(index, JSON.stringify(item))
+  })
+}
 
 // gets users data from local storage and returns an array of objects
 function downloadContact (storage) {
@@ -129,9 +87,9 @@ function downloadContact (storage) {
   return arr
 }
 
-function getTableRowUser (obj, checkbox) {
+function getTableRowUser (obj, checkbox, index) {
   let row = '<tr>'
-  if (checkbox) row += '<td>' + checkbox + '</td>'
+  if (checkbox) row += '<td>' + '<input type="checkbox" value="' + index + '">' + '</td>'
   row += '<td>' + obj['name'] + '</td>' +
     '<td>' + obj['phone'] + '</td>' +
     '<td>' + obj['email'] + '</td>' +
@@ -143,18 +101,31 @@ function enableEdit (e) {
   e.target.contentEditable = true
 }
 
-let table = usersArr.reduce((acc, user) => {
+let mainTable = arrayContact.reduce((acc, user) => {
   acc += getTableRowUser(user)
   return acc
 }, '')
 
-let tableCheckbox = usersArr.reduce((acc, user) => {
-  acc += getTableRowUser(user, checkbox)
+let tableCheckbox = arrayContact.reduce((acc, user, index) => {
+  acc += getTableRowUser(user, isCheckbox, index)
   return acc
 }, '')
 
-tableSearch.innerHTML = table
-tableEdit.innerHTML = table
+function checkboxHandler (e) {
+  if (e.target.type === 'checkbox' && e.target.checked) {
+    console.log(e.target.value)
+  }
+}
+
+// update DOM tables
+tableSearch.innerHTML = mainTable
+tableEdit.innerHTML = mainTable
 tableDelete.innerHTML = tableCheckbox
 
+// tables listeners
 tableEdit.addEventListener('click', enableEdit)
+tableDelete.addEventListener('click', checkboxHandler)
+
+// buttons listeners
+btnAddContact.addEventListener('click', addContact)
+btnSaveEdit.addEventListener('click', saveEdit)
